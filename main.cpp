463048ee4c97f850cc18677a3d46e5d76cf21499
LitoyN <litoynau@gmail.com>
2015-05-03 12:53:08 -0400

    Inits
    Created Netbeans project

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: elliot
+ *
+ * Created on May 3, 2015, 12:48 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
