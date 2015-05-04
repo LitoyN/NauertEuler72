7f243034295826968c997797afe657d4116ac39b
LitoyN <litoynau@gmail.com>
2015-05-03 20:46:05 -0400

    Phi function
    Phi function makes sense on paper, but only works for some values as
    implemented

--- main.cpp
+++ main.cpp
@@ -6,13 +6,76 @@
  */
 
 #include <cstdlib>
+#include <iostream>
+#include <math.h>
+
 
 using namespace std;
+
+int *primes = new int[78500];
+int primeCounter = 0;
+
+int primeSieve(int upperBound){
+    
+    int upperBoundSquareRoot = (int)sqrt((double)upperBound);
+    bool *isComposite = new bool[upperBound + 1];
+    primeCounter = 0;
+
+    memset(isComposite, 0, sizeof(bool) * (upperBound + 1));
+    for (int m = 2; m <= upperBoundSquareRoot; m++) {
+          if (!isComposite[m]) {
+                primes[primeCounter] = m;
+                primeCounter++;
+                for (int k = m * m; k <= upperBound; k += m)
+                      isComposite[k] = true;
+          }
+    }
+    for (int m = upperBoundSquareRoot; m <= upperBound; m++)
+          if (!isComposite[m]){
+                primes[primeCounter] = m;
+                primeCounter++;
+          }
+    delete [] isComposite;
+    cout << endl << "Number of Primes: " << primeCounter << endl;
+
+}
+
+int phi(int number){
+    
+    int phiValue = number - 1;
+    int numberSR = ceil(sqrt(number));
+    //cout << numberSR << endl;
+    
+    
+
+    for(int i = 2; i <= numberSR; i++){
+        if(number % i == 0){
+            if(number/i == numberSR){
+                return i * i - i;
+            }
+            //cout << i << endl;
+            //cout << number/i << endl;
+            cout << (i-1) * phi(number/i) << endl;
+            return i * phi(number/i);
+        }
+    }
+    
+    return phiValue;
+}
 
 /*
  * 
  */
 int main(int argc, char** argv) {
+    
+    int upperLimit = 1000000;
+    
+    primeSieve(upperLimit);
+    
+    int number = 114;
+    int phiVal = phi(number);
+    
+    cout << "phi value for " << number << ": " << phiVal;
 
     return 0;
 }
